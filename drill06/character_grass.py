7252ce198186172809ac5b667a362b416247a845
SeongmiKim@homepc <sum980121@naver.com>
2022-09-21 15:31:48 +0900

    drill06
    추가 수정

--- drill06/character_grass.py
+++ drill06/character_grass.py
@@ -1,7 +1,5 @@
 from pico2d import *
-
 import math
-math.sin(270 / 360*2*math.pi)
 
 open_canvas()
 
@@ -9,7 +7,7 @@ grass = load_image('grass.png')
 character=load_image('character.png')
 
 x=0
-while(x<800):
+while(x<770):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x,90)
@@ -17,7 +15,7 @@ while(x<800):
     delay(0.01)
 
 y=90
-while(y<550):
+while(y<540):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(770,y)
@@ -26,7 +24,7 @@ while(y<550):
 
 x=770
 y=550
-while(100<x):
+while(30<x):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x,y)
@@ -35,11 +33,22 @@ while(100<x):
 
 x=10
 y=550
-while(10<y):
+while(90<y):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(10,y)
     y = y-2
     delay(0.01)
+
+x=400
+y=300
+while(x,y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(400,300)
+    math.sin(270 / 360*2*math.pi)
+    math.cos(270 / 360*2*math.pi)
+    delay(0.01)
+
 
 close_canvas()
