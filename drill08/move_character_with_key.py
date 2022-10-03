219335e47ec095d1153f266f6fc26a0b73b3b024
SeongmiKim@homepc <sum980121@naver.com>
2022-10-03 13:08:34 +0900

    drill08
    2020184041 김성미

---
+++ drill08/move_character_with_key.py
@@ -0,0 +1,90 @@
+from pico2d import *
+
+def handle_events():
+    global running
+    global dirx
+    global diry
+    global state
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        ###KEY DOWN
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                state = 1
+                dirx +=1
+            elif event.key == SDLK_LEFT:
+                state = 0
+                dirx -=1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+            if event.key == SDLK_UP:
+                if state % 2:
+                    state = 1
+                else:
+                    state = 0
+                diry +=1
+            elif event.key == SDLK_DOWN:
+                if state % 2:
+                    state = 1
+                else:
+                    state = 0
+                diry -=1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+        #### Key UP
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                state = 3
+                dirx =0
+            elif event.key == SDLK_LEFT:
+                state = 2
+                dirx =0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+            if event.key == SDLK_UP:
+                if state % 2:
+                    state = 3
+                else:
+                    state = 2
+                diry =0
+            elif event.key == SDLK_DOWN:
+                if state % 2:
+                    state = 3
+                else:
+                    state = 2
+                diry =0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+
+
+open_canvas()
+grass = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x = 800 // 2
+y = 600 //2
+frame = 0
+state = 2
+dirx = 0
+diry = 0
+
+while running:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, state * 100, 100, 100, x, y)
+    update_canvas()
+
+    handle_events()
+    frame = (frame + 1) % 8
+    x+=dirx*5
+    y+=diry*5
+    delay(0.01)
+
+close_canvas()
